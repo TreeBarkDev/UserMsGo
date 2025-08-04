1e01f438e580446a657411731d218875292e83f8
Joseph Plant <jwppublic@gmail.com>
2025-08-04 14:44:44 +0100

    Remove values

--- internal/config/config.go
+++ internal/config/config.go
@@ -11,9 +11,9 @@ type Config struct {
 // In Go, config is specified in environment variables to be configured at the deployment phase
 func Load() Config {
 	return Config{
-		Port:          getEnv("PORT", "8082"),
-		CassandraHost: getEnv("CASSANDRA_HOST", "127.0.0.1"),
-		Keyspace:      getEnv("CASSANDRA_KEYSPACE", "demo"),
+		Port:          getEnv("PORT", ""),
+		CassandraHost: getEnv("CASSANDRA_HOST", ""),
+		Keyspace:      getEnv("CASSANDRA_KEYSPACE", ""),
 	}
 }
 
